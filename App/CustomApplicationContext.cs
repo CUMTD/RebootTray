8fe8754fa7986914bd2847e3de60685986a8b8d5
Ryan Blackman <rblackman24@gmail.com>
2016-06-01 12:09:30 -0500

    Add right click context menu

--- App/CustomApplicationContext.cs
+++ App/CustomApplicationContext.cs
@@ -8,31 +8,39 @@ namespace RebootTray.App
     internal class CustomApplicationContext : ApplicationContext
     {
         private NotifyIcon NotifyIcon { get; set; }
+        private ContextMenuStrip ContextMenu { get; set; }
 
         public CustomApplicationContext()
         {
             var components = new Container();
+
+            // create a new context menu
+            ContextMenu = new ContextMenuStrip();
+
+            // add an item for closing the application
+            var closeMenuItem = new ToolStripMenuItem("Exit")
+            {
+                Name = "CloseMenuItem",
+                Size = new Size(100, 22),
+                Text = "Exit"
+            };
+            closeMenuItem.Click += CloseMenuItem_Click;
+
+            // add close to the context menu
+            ContextMenu.SuspendLayout();
+            ContextMenu.Items.AddRange(new ToolStripItem[] { closeMenuItem });
+            ContextMenu.Name = "ContextMenu";
+            ContextMenu.Size = new Size(110, 70);
+            ContextMenu.ResumeLayout(false);
+
             NotifyIcon = new NotifyIcon(components)
             {
-                ContextMenuStrip = new ContextMenuStrip(),
+                ContextMenuStrip = ContextMenu,
                 Icon = new Icon("reboot.ico"),
                 Text = "Click to copy a command to reboot at 4AM.",
                 Visible = true
             };
             NotifyIcon.MouseUp += notifyIcon_MouseUp;
-        }
-
-        private void notifyIcon_MouseUp(object sender, EventArgs e)
-        {
-            var now = DateTime.Now;
-            var then = GetNext4AM(now);
-            var timespan = then - now;
-            var seconds = Math.Round(timespan.TotalSeconds);
-            var text = $"shutdown /r /t {seconds}";
-            Clipboard.SetText(text);
-            NotifyIcon.ShowBalloonTip(1000,
-                "Shutdown Command Coppied",
-                $"The shutdown command \"{text}\" was coppied to your clipboard. When run, this will reboot your computer at 4:00 AM.", ToolTipIcon.Info);
         }
 
         private static DateTime GetNext4AM(DateTime now)
@@ -45,11 +53,43 @@ namespace RebootTray.App
             return new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, 4, 0, 0);
         }
 
+        #region Event Handlers
+
+        private static void CloseMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void notifyIcon_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                var now = DateTime.Now;
+                var then = GetNext4AM(now);
+                var timespan = then - now;
+                var seconds = Math.Round(timespan.TotalSeconds);
+                var text = $"shutdown /r /t {seconds}";
+                Clipboard.SetText(text);
+                NotifyIcon.ShowBalloonTip(1000,
+                    "Shutdown Command Coppied",
+                    $"The shutdown command \"{text}\" was coppied to your clipboard. When run, this will reboot your computer at 4:00 AM.",
+                    ToolTipIcon.Info);
+            }
+        }
+
+        #endregion Event Handlers
+
+        #region IDisposable
+
         protected override void Dispose(bool disposing)
         {
+            ContextMenu.Dispose();
+            ContextMenu = null;
             NotifyIcon.Dispose();
             NotifyIcon = null;
             base.Dispose(disposing);
         }
+
+        #endregion IDisposable
     }
 }
